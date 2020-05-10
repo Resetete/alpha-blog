a82114a4821ffe9c51f92780355c268f3dc52984
Theresa <tmannschatz@gmail.com>
2020-05-10 22:43:19 +0200

    add index page with all listed articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id]) # find the article with id from url
   end
+
+  def index
+    @articles = Article.all
+  end
 end
