21dba1e297b3d6c10beee17fc87767ea8e077b61
Theresa <tmannschatz@gmail.com>
2020-05-11 23:39:37 +0200

    add edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     # create new instance variable from Article based on the params information; params need to have the top level key :article and from it we allow the attributes :title, :description to be stored
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -20,6 +24,16 @@ class ArticlesController < ApplicationController
       redirect_to @article # rails extracts the id from this instance variable
     else
       render 'new' # render new template again
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 end
