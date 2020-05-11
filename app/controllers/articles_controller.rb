6e253c914159f8e3cd7b0bab33a06e989bbcc2c0
Theresa <tmannschatz@gmail.com>
2020-05-11 13:43:23 +0200

    add create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    # create new instance variable from Article based on the params information; params need to have the top level key :article and from it we allow the attributes :title, :description to be stored
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save 
+    redirect_to articles_path(@article) # rails extracts the id from this instance variable
   end
 
   def new
