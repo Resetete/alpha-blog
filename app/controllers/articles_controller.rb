a031e1c37dbf95e7ad51b5eac00e5ce25e366921
Theresa <tmannschatz@gmail.com>
2020-05-27 22:00:16 +0200

    Hardcode user id associations for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,7 @@ class ArticlesController < ApplicationController
   def create
     # create new instance variable from Article based on the params information; params need to have the top level key :article and from it we allow the attributes :title, :description to be stored
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = 'Article successful saved'
       redirect_to @article # rails extracts the id from this instance variable
