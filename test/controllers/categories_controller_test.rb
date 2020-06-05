4a5de75c0f5e25902e03fbe042653469150175e4
Theresa <tmannschatz@gmail.com>
2020-06-05 23:14:40 +0200

    Add model, controller and integration tests for categories

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @category = Category.create(name: 'sports')
+  end
+
+  test "should get index" do
+    get categories_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_url
+    assert_response :success
+  end
+
+  test "should show category" do
+    get category_url(@category)
+    assert_response :success
+  end
+
+  test "should create category"  do
+    assert_difference('Category.count', 1) do
+      post categories_url, params: { category: { name: 'travel' } }
+    end
+
+    assert_redirected_to category_url(Category.last)
+  end
+
+end
