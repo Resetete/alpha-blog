f0d656cea88312ff48985fe7aa4173cfa86e68d6
Theresa <tmannschatz@gmail.com>
2020-06-06 21:00:05 +0200

    Add admin restrictions to categories; tests: login as admin user

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @category = Category.create(name: 'sports')
+    @category = Category.create(name: 'Sports')
+    @admin_user = User.create(username: 'admin', email: 'admin@example.com',
+    password: '123456', admin: true) # simulate an admin user
   end
 
   test "should get index" do
@@ -11,6 +13,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@admin_user)
     get new_category_url
     assert_response :success
   end
@@ -21,11 +24,20 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create category"  do
+    sign_in_as(@admin_user)
     assert_difference('Category.count', 1) do
-      post categories_url, params: { category: { name: 'travel' } }
+      post categories_url, params: { category: { name: 'Travel' } }
     end
 
     assert_redirected_to category_url(Category.last)
+  end
+
+  test "should not create category if not admin" do
+    assert_no_difference('Category.count') do
+      post categories_url, params: { category: { name: 'Travel' } }
+    end
+
+    assert_redirected_to categories_url
   end
 
 end
