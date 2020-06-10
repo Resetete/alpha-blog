fa4530b691e2f73a30f07f46779ad6f71d956d14
Theresa <tmannschatz@gmail.com>
2020-06-10 21:13:01 +0200

    Add user sign up test

--- test/integration/sign_up_user_test.rb
+++ test/integration/sign_up_user_test.rb
@@ -4,7 +4,6 @@ class SignUpUserTest < ActionDispatch::IntegrationTest
   setup do
     @admin_user = User.create(username: 'admin', email: 'admin@example.com', password: '123456', admin: true)
     @regular_user = User.create(username: 'user1', email: 'user1@example.com', password: '123456', admin: false)
-
   end
 
   test 'should sign in as regular user' do
@@ -20,5 +19,17 @@ class SignUpUserTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_match 'Logged in successully', response.body
     assert_match '(Admin)', response.body
+  end
+
+  test 'get signup for and successully sign up user' do
+    get signup_path
+    assert_response :success
+    assert_difference 'User.count',1 do
+      post users_path, params: { user: { username: 'New test user', email: 'testuser@example.com', password: '123456' } }
+      assert_response :redirect
+    end
+    follow_redirect!
+
+    assert_match 'New test user, Welcome', response.body
   end
 end
